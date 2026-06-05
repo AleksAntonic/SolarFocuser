4b20667dc59d1e7371797bfcb0b97a31cc2ad13f
AleksAntonic <aantonic@ethz.ch>
2026-06-06 00:16:54 +0200

    switching to RL

--- solarfocuser/src/controller.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import numpy as np
-import gymnasium as gym
-
-class Controller(ABC):
-    @abstractmethod
-    def get_action(self, state, env) -> np.ndarray:
-        pass
